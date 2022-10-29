ffe518e7962521d30a8ba0d98ed5c02fa9cb4b0b
Samuel Park <s.park111497@gmail.com>
2022-10-29 11:20:32 -0700

    finished assignment

--- sql-practice/seed-data.sql
+++ sql-practice/seed-data.sql
@@ -26,3 +26,9 @@ VALUES ('The Falling Box', 1, 2015, 25000),
   SELECT * FROM albums WHERE year IS BETWEEN 2018 AND 2020;
 
   SELECT * FROM albums WHERE band_id IN (1, 3, 4);
+
+  SELECT * FROM albums WHERE title LIKE "The%";
+
+  SELECT * FROM albums ORDER BY num_sold DESC LIMIT 2;
+
+  SELECT * FROM albums ORDER BY num_sold DESC LIMIT 2 OFFSET 2;
